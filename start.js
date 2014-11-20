8f5877cb5b0d4794a62ae827b32d7af7418e926c
Ben Drucker <bvdrucker@gmail.com>
2014-11-20 11:50:02 -0500

    Add NewRelic tracing

--- start.js
+++ start.js
@@ -1,3 +1,11 @@
+'use strict';
+
+var config = require('./config');
+
+if (config.get('NODE_ENV') === 'production') {
+  require('newrelic');
+}
+
 require('./src/server').start(function () {
   console.log('server running');
 });
